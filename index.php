4728752bdac423da3e1e9e8ebc7d0fd9ad2c64cb
ducviethaboo <70750327+ducviethaboo@users.noreply.github.com>
2020-10-20 16:06:33 +0700

    Commit 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$array = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+
+function bubbleSort($arr)
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        for($j = 0; $j < count($arr) - $i - 1; $j++) {
+            if($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+
+print_r(bubbleSort($array));
